4b536b1b109a3bf1da1d9de181026952945e2029
Itamar Laufer <35191767+itamarLaufer@users.noreply.github.com>
2018-02-28 11:36:06 +0200

    Update Move.java

--- Move.java
+++ Move.java
@@ -4,6 +4,6 @@ package com.company;
  * represents a move in the game
  * state of this move suppose to get this move and apply it in the state or to undo it.
  */
-public class Move
+public abstract class Move
 {
 }

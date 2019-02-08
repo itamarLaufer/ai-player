98cf08f58834e43ef49584686255ed95d5be889f
Itamar Laufer <35191767+itamarLaufer@users.noreply.github.com>
2019-02-08 16:15:17 +0200

    Delete Move.java

--- Move.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/**
- * represents a move in the game
- * state of this move suppose to get this move and apply it in the state or to undo it.
- */
-public abstract class Move
-{
-}
